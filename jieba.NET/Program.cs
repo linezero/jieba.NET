3f44f600b9ed40359102869fc481110f8ee22401
linezero <linezero@qq.com>
2017-03-17 14:36:46 +0800

    Update

--- jieba.NET/Program.cs
+++ jieba.NET/Program.cs
@@ -9,9 +9,24 @@ namespace jieba.NET
         static void Main(string[] args)
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            TestDemo test = new TestDemo();
-            test.CutDemo();
-            test.TokenizeDemo();
+            //TestDemo test = new TestDemo();
+            //test.CutDemo();
+            //test.TokenizeDemo();
+            var segmenter = new JiebaSegmenter();
+            var segments = segmenter.Cut("我来到北京清华大学", cutAll: true);
+            Console.WriteLine("【全模式】：{0}", string.Join("/ ", segments));
+
+            segments = segmenter.Cut("我来到北京清华大学");  // 默认为精确模式
+            Console.WriteLine("【精确模式】：{0}", string.Join("/ ", segments));
+
+            segments = segmenter.Cut("他来到了网易杭研大厦");  // 默认为精确模式，同时也使用HMM模型
+            Console.WriteLine("【新词识别】：{0}", string.Join("/ ", segments));
+
+            segments = segmenter.CutForSearch("小明硕士毕业于中国科学院计算所，后在日本京都大学深造"); // 搜索引擎模式
+            Console.WriteLine("【搜索引擎模式】：{0}", string.Join("/ ", segments));
+
+            segments = segmenter.Cut("结过婚的和尚未结过婚的");
+            Console.WriteLine("【歧义消除】：{0}", string.Join("/ ", segments));
             Console.ReadKey();
         }
     }
